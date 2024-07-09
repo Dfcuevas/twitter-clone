a5acd3b0247af5e09433b7b265b2a4fb6309533c
Diego Cuevas <ingdcuevas@gmail.com>
2024-07-09 10:29:12 -0500

    3- Auth handled and routes protected in client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // esto inhabilita el hecho de hacer el fetch de los datos cuando cambiamos de pestañas en el navegador y volvemos a la del proyecto.
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
